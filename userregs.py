595bc73ca1de2c1022adc39bb686a6105ede56b9
PaomFarv <omor.faruk.myself@gmail.com>
2025-01-19 21:00:52 +0600

    Updated

--- userregs.py
+++ userregs.py
@@ -8,6 +8,7 @@ user_infos = []
 
 def sign_in():
     global user_infos
+    global email_bar,pw_bar
     user_input = email_bar.get()
     pw = pw_bar.get()
     user_infos.append(pw)
@@ -37,6 +38,7 @@ def home_page():
     headline = ctk.CTkLabel(master=main_frame,text="Gateway",font=("Bahnschrift SemiBold",80))
     headline.pack(pady=20)
 
+    global email_bar,pw_bar
     email_bar = ctk.CTkEntry(master=main_frame,placeholder_text="Email",font=("Arial",15),height=35,width=270)
     email_bar.pack(pady=5)
 
@@ -52,8 +54,12 @@ def home_page():
     signin_btn = ctk.CTkButton(master=main_frame,text="Sign In",font=("Bahnschrift SemiBold",20),height=40,width=100,command=sign_in)
     signin_btn.pack(pady=5)
 
+def popup_disable():
+    feedback.configure(text="")
+
 def login():
     global user_infos
+    global email_bar,pw_bar
     user_input = email_bar.get()
     pw = pw_bar.get()
 
@@ -62,19 +68,24 @@ def login():
     if not pw.strip():
         return
 
-    if user_input and pw not in user_infos:
+    if user_input in user_infos and pw in user_infos:   
+        for widget in main_frame.winfo_children():
+            widget.destroy()
+
+        label_1 = ctk.CTkLabel(master=main_frame,text="Login Successful.",font=("Bahnschrift SemiBold",40))
+        label_1.pack(pady=100)
+
+        back_btn = ctk.CTkButton(master=main_frame,text="Go Back",font=("Bahnschrift SemiBold",20),height=40,width=100,command=home_page)
+        back_btn.pack(pady=5)
+
+        label_0 = ctk.CTkLabel(master=main_frame,text="You're all catch up for now.",font=("Bahnschrift SemiBold",15))
+        label_0.pack(pady=50)
+    else:
+        global feedback
         feedback = ctk.CTkLabel(master=main_frame,text="User Not Found.",font=("Arial",10),text_color="Red")
         feedback.pack()
+        root.after(3000,popup_disable)
         return
-    
-    for widget in main_frame.winfo_children():
-        widget.destroy()
-
-    label_1 = ctk.CTkLabel(master=main_frame,text="Login Successful.",font=("Bahnschrift SemiBold",40))
-    label_1.pack(pady=100)
-
-    label_0 = ctk.CTkLabel(master=main_frame,text="You're all catch up for now.",font=("Bahnschrift SemiBold",15))
-    label_0.pack(pady=50)
 
 
 main_frame = ctk.CTkFrame(master=root)
